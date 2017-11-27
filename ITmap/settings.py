a5a006d2b79efcca982def95e16ec32e0e26cf83
antonyq <antonyq@ukr.net>
2017-11-27 16:30:46 +0200

    master

--- ITmap/settings.py
+++ ITmap/settings.py
@@ -2,8 +2,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
