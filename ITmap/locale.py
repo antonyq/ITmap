9f8a7109a3311c577a56e05188b8f936fb934a05
antonyq <antonyq@ukr.net>
2017-12-31 03:15:22 +0200

    master

--- ITmap/locale.py
+++ ITmap/locale.py
@@ -4,6 +4,6 @@ from json import load
 def localize(language, file_names):
     locales = []
     for name in file_names:
-        with open(f'static/locale/{language}/{name}.json', encoding='utf8') as data_json:    
+        with open('static/locale/' + language + '/' + name + '.json', encoding='utf8') as data_json:    
             locales.append(load(data_json))
     return locales
